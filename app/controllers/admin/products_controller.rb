1bc09b6f0fe1e9c59fc6ac17874d99fcdab30b2f
ali mercade <alimercad@gmail.com>
2024-09-06 15:22:54 +0100

    Product views/controllers

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,20 +1,25 @@
-class Admin::ProductsController < ApplicationController
+class Admin::ProductsController < AdminController
   before_action :set_admin_product, only: %i[ show edit update destroy ]
 
+  # GET /admin/products or /admin/products.json
   def index
     @admin_products = Product.all
   end
 
+  # GET /admin/products/1 or /admin/products/1.json
   def show
   end
 
+  # GET /admin/products/new
   def new
     @admin_product = Product.new
   end
 
+  # GET /admin/products/1/edit
   def edit
   end
 
+  # POST /admin/products or /admin/products.json
   def create
     @admin_product = Product.new(admin_product_params)
 
@@ -29,18 +34,22 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  # PATCH/PUT /admin/products/1 or /admin/products/1.json
   def update
-    respond_to do |format|
-      if @admin_product.update(admin_product_params)
-        format.html { redirect_to admin_product_url(@admin_product), notice: "Product was successfully updated." }
-        format.json { render :show, status: :ok, location: @admin_product }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @admin_product.errors, status: :unprocessable_entity }
+    @admin_product = Product.find(params[:id])
+    if @admin_product.update(admin_product_params.reject { |k| k["images"] })
+      if admin_product_params["images"]
+        admin_product_params["images"].each do |image|
+          @admin_product.images.attach(image)
+        end
       end
+      redirect_to admin_products_path, notice: "Product updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
+  # DELETE /admin/products/1 or /admin/products/1.json
   def destroy
     @admin_product.destroy!
 
@@ -51,12 +60,13 @@ class Admin::ProductsController < ApplicationController
   end
 
   private
-
+    # Use callbacks to share common setup or constraints between actions.
     def set_admin_product
       @admin_product = Product.find(params[:id])
     end
 
+    # Only allow a list of trusted parameters through.
     def admin_product_params
-      params.require(:product).permit(:name, :description, :price, :category_id, :active)
+      params.require(:product).permit(:name, :description, :price, :category_id, :active, images: [])
     end
 end
